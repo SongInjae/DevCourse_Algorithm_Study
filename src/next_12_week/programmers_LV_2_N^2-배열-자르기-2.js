4adaa1710c126dfc18d426e619eca67d53b45dd0
Suyeon <msjgr2801@gmail.com>
2024-03-02 16:06:49 +0900

    금~토일 문제

---
+++ src/next_12_week/programmers_LV_2_N^2-배열-자르기-2.js
@@ -0,0 +1,36 @@
+// ! 2, 15, 16, 17, 18, 19 20 실패
+
+function solution(n, left, right) {
+  const answer = [];
+  let [startRow, startCol] = [Math.floor(left / n), left % n];
+  let [endRow, endCol] = [Math.floor(right / n), right % n];
+
+  for (let row = startRow; row <= endRow; row++) {
+    const nums = [...new Array(n)].map((_, index) => {
+      if (index <= row) {
+        return row + 1;
+      }
+      return index + 1;
+    });
+    if (row === startRow) {
+      for (
+        let col = startCol;
+        startRow !== endRow ? col < n : col <= endCol;
+        col++
+      ) {
+        answer.push(nums[col]);
+      }
+    } else if (row < endRow) {
+      for (let col = 0; col < n; col++) {
+        answer.push(nums[col]);
+      }
+    } else {
+      for (let col = 0; col <= endCol; col++) {
+        answer.push(nums[col]);
+      }
+    }
+  }
+  return answer;
+}
+
+solution(3, 3, 4);
